3f77104806bd668629298e42355a09b564e4c920
Laptop <rn7cvj-dvfu>
2024-03-19 20:30:17 +1000

    init

---
+++ lab1.sql
@@ -0,0 +1,25 @@
+
+# 2
+
+select * 
+from sales
+where "date"='2022-02-19';
+
+# 3
+
+select *
+from writeoffs
+limit 100;
+
+# 4
+
+select DISTINCT "date"
+from sales;
+
+# 5
+
+
+select max(amount * price_per_one) as "Максимальная сумма",
+       min(amount * price_per_one) as "Минимальная сумма",
+       count(amount) as num
+from sales
